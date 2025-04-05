079c1c011b8c6ca19b8370a930c338c0ea005982
Kilexy <danielibanezbenavente57@gmail.com>
2025-04-05 11:43:32 +0200

    Update check_punto_carga.py

--- check_punto_carga.py
+++ check_punto_carga.py
@@ -3,7 +3,8 @@ import requests
 PUNTO_ID = 97896
 URL = "https://www.iberdrola.es/o/webclipb/iberdrola/puntosrecargacontroller/getDatosPuntoRecarga"
 HEADERS = {
-    "Content-Type": "application/json"
+    'Content-Type': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
 }
 PAYLOAD = {
     "dto": {"cuprId": [PUNTO_ID]},
