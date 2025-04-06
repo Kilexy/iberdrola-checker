93f7b77cb9d85b44d38adac22c7bde7bfdd4e2b7
Kilexy <danielibanezbenavente57@gmail.com>
2025-04-06 13:49:52 +0200

    Update check_iberdrola.py

--- check_iberdrola.py
+++ check_iberdrola.py
@@ -83,7 +83,7 @@ def consultar_punto():
 # 🔁 MAIN LOOP
 notificado = False
 tiempo_espera = 5  # segundos
-ciclos = 120        # total 10 minutos
+ciclos = 110        # total 10 minutos
 
 print("🚀 Iniciando verificación de disponibilidad...")
 for i in range(ciclos):
